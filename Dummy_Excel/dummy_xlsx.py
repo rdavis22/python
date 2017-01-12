a352864e8466cf480fd0483e2b75a4af4ef5c70e
Richard Davis <rcdavis39@gmail.com>
2017-01-12 10:20:42 -0600

    updated 'dummy xlsx'

--- Dummy_Excel/dummy_xlsx.py
+++ Dummy_Excel/dummy_xlsx.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python
+
+##Working and copying from the 'openpyxl' source page at...
+#'https://openpyxl.readthedocs.io/en/default'
+
+
+import pip
+#'openpyxl' is the external module to install
+def install(openpyxl):
+    pip.main(['install', openpyxl])
+
+from openpyxl import Workbook
+wb=Workbook()
+
+#grab the active worksheet
+ws=wb.active
+
+#Data can be assigned directly to the cells
+ws['A1']=42
+
+#Rows can also be appended
+ws.append([1, 2, 3])
+
+#Python types will automatically be converted
+import datetime
+ws['A2']=datetime.datetime.now()
+
+#save the file
+wb.save("sample.xlsx")
